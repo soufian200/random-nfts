40e4189bcd969c9439706a6e16960da3d9e267e1
mrx <soufianxlm@gmail.com>
2022-02-27 16:24:02 +0100

    add 404&500 pages

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return <div className="bg-slate-800 h-screen w-screen flex justify-center items-center ">
+    <div className="flex justify-center items-center text-slate-200">
+      <h1 className="font-bold text-4xl text-white">404 </h1><h1>  <span className="ml-3 mr-1">|</span> Page Not Found</h1>
+    </div>
+  </div>
+}
+
+export default NotFound
---
+++ pages/500.tsx
@@ -0,0 +1,9 @@
+const ServerDown = () => {
+  return <div className="bg-slate-800 h-screen w-screen flex justify-center items-center ">
+    <div className="flex justify-center items-center text-slate-200">
+      <h1 className="font-bold text-4xl text-white">500 </h1><h1>  <span className="ml-3 mr-1">|</span> Server-side error occurred</h1>
+    </div>
+  </div>
+}
+
+export default ServerDown
