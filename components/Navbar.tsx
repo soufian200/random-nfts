bd7bb894682d73a31e0b02b9438ff44f8c75e3f1
mrx <soufianxlm@gmail.com>
2022-03-04 21:46:54 +0100

    and new env var NEXT_PUBLIC_YOUTUBE_VIDEO_ID

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return <nav className='bg-slate-900 border-b border-slate-600 py-3 px-20 flex items-center justify-between w-full'>
         <Logo />
         <div className=''>
-            <a href={process.env.NEXT_PUBLIC_YOUTUBE_URL} target="_blank" rel="noopener" className='text-red-500 font-bold tracking-widest text-lg hover:text-red-600'> How It Works </a>
+            <a href={`https://www.youtube.com/watch?v=${process.env.NEXT_PUBLIC_YOUTUBE_VIDEO_ID}`} target="_blank" rel="noreferrer" className='text-red-500 font-bold tracking-widest text-lg hover:text-red-600'> How It Works </a>
         </div>
     </nav>
 }
